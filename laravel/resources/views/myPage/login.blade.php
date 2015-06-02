88aeee29d8320d1196d2d4a06aff58e2da01b025
lukas-moertl <lukas.moertl@gmx.at>
2015-06-02 13:04:29 +0200

    Login mit Funktionalität bestückt. Fehler. ToDo: Fehler beheben, neues Form für Lehrer schreiben

--- laravel/resources/views/myPage/login.blade.php
+++ laravel/resources/views/myPage/login.blade.php
@@ -1,16 +1,17 @@
-@extends('myPage.homemasterview')
-
+@extends('myPage.masterview')
+<!--- TODO: 2. login für lehrer -->
 <div class="padding">
     <h1>Log in</h1>
-    <p class="bold">Verwende deinen Benutzernamen und dein Passwort wenn du ein Schüler bist.<br>Bist du ein Lehrer, verwende deine Emailadresse<br>Wenn du dein Passwort vergessen hast, klick hier</p>
-    
-        <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></span>
-  <input type="text" class="form-control" placeholder="Benutzername/Emailadresse" aria-describedby="basic-addon1">
-        </div>
-        <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></span>
-  <input type="password" class="form-control" placeholder="password" aria-describedby="basic-addon1">
-        </div>
-        <button type="button" class="btn btn-primary btn-block">Login</button>
+    <p class="bold">Verwende deinen Fischnamen und dein Passwort</p>
+    <form action="{{ action('Auth/LoginController@postStudent', this) }}" method="post">   
+      <div class="input-group">
+        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></span>
+        <input name="fishname" type="text" class="form-control" placeholder="Benutzername/Emailadresse" aria-describedby="basic-addon1">
+      </div>
+      <div class="input-group">
+        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></span>
+        <input name="password" type="password" class="form-control" placeholder="password" aria-describedby="basic-addon1">
+      </div>
+      <button type="submit" class="btn btn-primary btn-block">Login</button>
+    </form>
 </div>
